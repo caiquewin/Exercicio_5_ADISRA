8003640f42073cfbad8c0e1c89ec9362b41c57d4
Caique Pereira Alves <caiquewin@gmail.com>
2020-09-11 17:07:13 -0300

    Projeto criado e finalizado

---
+++ Exercício 5/Number.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercício_5
+{
+    class Number
+    {
+        private int Num;
+        public Number(int number)
+        {
+            Num = number;
+        }
+        public int findGolomb(int n)
+        {
+
+            // base (condição)
+            if (n == 1)
+                return 1;
+
+            /* formula 
+            * 1° = n-1
+            * 2 vai chamar um loop até alcancar o numero 1;
+            * 3 para fechar o loop a condição precisa ser ingual a ==1
+            */
+            return 1 + findGolomb(n - findGolomb(findGolomb(n - 1)));
+        }
+
+        public void printGolomb()
+        {
+            for (int i = 1; i <= Num; i++)
+                Console.Write(findGolomb(i) + " ");
+        }
+    }
+}
---
+++ Exercício 5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercício_5
+{
+
+    class program
+    {
+        public static void Main()
+        {
+            Console.Write("Entrada: ");
+            Number number = new Number(int.Parse(Console.ReadLine()));
+            number.printGolomb();
+        }
+    }
+
+
+
+}
